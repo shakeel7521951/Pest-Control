090f55ade321c54850035f89efefcc9aafba6972
Muhammad Shakeel <shakeel7521951@gmail.com>
2025-07-21 12:01:46 +0500

    add page routes

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/common/navbar/page";
+import Footer from "./components/common/footer/page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
