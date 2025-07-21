74c606b923cb9fb25954d6a035449f05d58d1429
Muhammad Shakeel <shakeel7521951@gmail.com>
2025-07-21 12:21:58 +0500

    add footer and navbar

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/common/navbar/page";
-import Footer from "./components/common/footer/page";
+import Navbar from "./components/common/Navbar";
+import Footer from "./components/common/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
